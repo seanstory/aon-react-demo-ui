d751daef9150a28fa29841d05487aba1fd5eb435
Sean Story <sean.j.story@gmail.com>
2021-01-03 15:22:17 -0600

    Updates

---
+++ precache-manifest.b619ac8669ec93e1d3ca99451827ab71.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d196651db9931f90882cd8f929cb4165",
+    "url": "/aon-react-demo-ui/index.html"
+  },
+  {
+    "revision": "6a2d3fd65b7bad702ed3",
+    "url": "/aon-react-demo-ui/static/css/main.3847cbad.css"
+  },
+  {
+    "revision": "6a2d3fd65b7bad702ed3",
+    "url": "/aon-react-demo-ui/static/js/main.f1212743.js"
+  },
+  {
+    "revision": "95ea6e06508ae6cb347ab110e0d2cd19",
+    "url": "/aon-react-demo-ui/static/js/main.f1212743.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aon-react-demo-ui/precache-manifest.e8dee5fa5be73d79bf77eb6f9be6ffb5.js"
+  "/aon-react-demo-ui/precache-manifest.b619ac8669ec93e1d3ca99451827ab71.js"
 );
 
 self.addEventListener('message', (event) => {
